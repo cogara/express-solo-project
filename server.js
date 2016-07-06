99d775af10e292b71de8b0f66ca60b1eb1b16974
Connor O'Gara <ogara.connor@gmail.com>
2016-07-06 15:12:41 -0500

    commit change

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
+var fun = require('./fun.js');
 
 var port = 3000;
 
+
+
 app.listen(port, function(request, response) {
-  console.log('Hello World!');
+  console.log('Listening to World!');
 })
+
+app.use('/', fun);
